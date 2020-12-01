74eea7cae9980cd6bcedc4ed9836a9d963d4312a
samboxft <30531932+samboxft@users.noreply.github.com>
2020-12-01 08:56:30 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+$("#container").append(`<center> 
+<table>
+    <h4>Skills</h4>
+
+    
+        <tr>
+            <td>
+                <table>
+                    <tr>
+                        <td>Html</td>
+                        <td>\u2B50\u2B50\u2B50\u2B50\u2B50</td>
+                    </tr>
+                    <tr>
+                        <td>CSS</td>
+                        <td>\u2B50\u2B50\u2B50</td>
+                    </tr>
+                    <tr>
+                        <td>JavaScript</td>
+                        <td>\u2B50\u2B50\u2B50</td>
+                    </tr>
+                </table>
+            </td>
+            <td>
+                <table >
+                    <tr>
+                        <td>Jquery</td>
+                        <td>\u2B50\u2B50\u2B50\u2B50\u2B50</td>
+                    </tr>
+                    <tr>
+                        <td>Ajax</td>
+                        <td>\u2B50\u2B50\u2B50</td>
+                    </tr>
+                    <tr>
+                        <td>TypeScript</td>
+                        <td>\u2B50\u2B50\u2B50</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    
+</table></center>`)
